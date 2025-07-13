246f61f7fef888d1cb64b525f50c8deae48f7d0f
Grumbled0rf <grumbled0rf@pm.me>
2025-07-13 11:03:18 +0000

    first commit

---
+++ lms/public/frontend/assets/InsertLink-DzqyzKdz.js
@@ -0,0 +1,2 @@
+import{as as t,be as s,bf as a,bg as i}from"./index--jEUqk-V.js";const c=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{c as default};
+//# sourceMappingURL=InsertLink-DzqyzKdz.js.map
